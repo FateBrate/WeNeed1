0930343c001c83c6b615175e89fc0bf080c46fda
FateBrate <drek.fatih@live.com>
2025-04-06 16:00:59 +0200

    add status and workhours

---
+++ WeNeed1/WeNeed1.Service/Migrations/20250328124302_workHours.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeNeed1.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class workHours : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "EndTime",
+                table: "SportsCenters",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "StartTime",
+                table: "SportsCenters",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "SportsCenters");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "SportsCenters");
+        }
+    }
+}
---
+++ WeNeed1/WeNeed1.Service/Migrations/20250402230146_reservationStatuses.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeNeed1.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class reservationStatuses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CancellationReason",
+                table: "Reservations",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Reservations",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CancellationReason",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Reservations");
+        }
+    }
+}

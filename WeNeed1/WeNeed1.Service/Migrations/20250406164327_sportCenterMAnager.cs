399f83c4f43ace231ab5bfe375657c9f4e925be8
FateBrate <drek.fatih@live.com>
2025-04-13 17:13:11 +0200

    migrations

---
+++ WeNeed1/WeNeed1.Service/Migrations/20250406164327_sportCenterMAnager.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeNeed1.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class sportCenterMAnager : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ManagerId",
+                table: "SportsCenters",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SportsCenters_ManagerId",
+                table: "SportsCenters",
+                column: "ManagerId",
+                unique: true,
+                filter: "[ManagerId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SportsCenters_Users_ManagerId",
+                table: "SportsCenters",
+                column: "ManagerId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SportsCenters_Users_ManagerId",
+                table: "SportsCenters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SportsCenters_ManagerId",
+                table: "SportsCenters");
+
+            migrationBuilder.DropColumn(
+                name: "ManagerId",
+                table: "SportsCenters");
+        }
+    }
+}

0c548ddd093d52235a14ddb9a30ef36b2e263cf2
FateBrate <drek.fatih@live.com>
2025-04-29 00:50:40 +0200

    add default image for sport centers

---
+++ WeNeed1/WeNeed1.Service/Migrations/20250426165620_teamsportimages.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeNeed1.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class teamsportimages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "TeamPicture",
+                table: "Teams",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Image",
+                table: "SportsFields",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Image",
+                table: "SportsCenters",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TeamPicture",
+                table: "Teams");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "SportsFields");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "SportsCenters");
+        }
+    }
+}

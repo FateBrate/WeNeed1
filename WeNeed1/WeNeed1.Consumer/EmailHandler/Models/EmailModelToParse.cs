5d8375c047de01d8ea0b3a15fee43d7748cd2703
FateBrate <drek.fatih@live.com>
2025-06-04 01:04:52 +0200

    add rabbit mq service and email

---
+++ WeNeed1/WeNeed1.Consumer/EmailHandler/Models/EmailModelToParse.cs
@@ -0,0 +1,8 @@
+﻿namespace WeNeed1.Consumer;
+
+public class EmailModelToParse
+{
+    public string? Recipient { get; set; }
+    public string? Subject { get; set; }
+    public string? Content { get; set; }
+}
